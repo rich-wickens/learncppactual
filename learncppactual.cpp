f5db60c92c35f459ae23f16429381bf97fa0dc4a
Rich Wickens <rwickens100@gmail.com>
2024-09-18 14:55:34 +0100

    Lesson on cout and cin, 1.5

--- learncppactual.cpp
+++ learncppactual.cpp
@@ -2,10 +2,21 @@
 //
 
 #include <iostream>
+int a{ 5 };
 
+// Function that requests three numbers to be entered by a user.
+// Numbers that the user enters will then be displayed on the console.
 int main()
 {
-    std::cout << "Hello World!\n";
+    int x{ 5 };
+    int y{};
+
+    std::cout << "Hello World!\n";                                          // new line without flushing the buffer
+    std::cout << "My integer x is currently " << x << "." << std::endl;     // new line with manual flush - I am done here so this makes sense
+    std::cout << "Now it's your turn to enter numbers:\n";
+    std::cin >> x;
+    std::cin >> y;                                                          // the user can enter these in one line seperated by a space or in two enter commands
+    std::cout << "You entered the numbers " << x << " and " << y << "!\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
