8c7e781693d2f67863fa980046bcc8d8dc5dd1c5
Rich Wickens <rwickens100@gmail.com>
2024-09-18 21:28:31 +0100

    finished

--- learncppactual.cpp
+++ learncppactual.cpp
@@ -3,8 +3,20 @@
 
 #include <iostream>
 
+
+// a function that:
+// 1. take user integer input in one line
+// 2. doubles the input and outputs it on the second line
+// 3. triples the input and outputs it on the third line
 int main()
 {
-	
+	std::cout << "Enter an integer: ";
+
+	int num{};
+	std::cin >> num;
+
+	std::cout << "Double that number is: " << num * 2 << '\n';
+	std::cout << "Triple that number is: " << num * 3 << '\n';
+
 	return 0;
 }
